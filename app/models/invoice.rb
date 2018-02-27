68e663a41d067e1a8a45b3d11a4a2e07ad01ee7f
yarik3351 <yarik3351@gmail.com>
2018-02-27 10:41:55 +0200

    invoices modify

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,6 @@ class Invoice < ApplicationRecord
   #belongs_to :user
   #belongs_to :customer
   validates :number, presence: true
-  enum status: [:new, :issued, :paid, :cancelled]
+  enum status: [:fresh, :issued, :paid, :cancelled]
   enum currency: [:USD, :EUR, :RUB, :UAH]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "invoices#index"
-
+  
   resources :invoices
   resources :users
   get '/login' => 'sessions#login'
